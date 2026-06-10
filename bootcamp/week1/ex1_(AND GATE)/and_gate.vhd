34d170f17d74a731d4f1d4b05a776bfc6b70e03f
Nuupur Deshmukh <nuupurd.2727@gmail.com>
2026-06-10 16:27:56 +0530

    Uploading and_gate.vhd

---
+++ bootcamp/week1/ex1_(AND GATE)/and_gate.vhd
@@ -0,0 +1,16 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity and_gate is
+	port(
+		a: in std_logic;
+		b:in std_logic;
+		y: out std_logic
+	);
+end and_gate;
+architecture rt1 of and_gate is
+begin
+	y<= a and b;
+end rt1;
+
+
